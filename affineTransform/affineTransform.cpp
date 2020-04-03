acb7b50d99b3ad362e4b706c2f97597145847930
Kill Toxic <913087364@qq.com>
2020-04-03 14:52:29 +0800

    affineTransform

--- affineTransform/affineTransform.cpp
+++ affineTransform/affineTransform.cpp
@@ -1,20 +1,31 @@
-﻿// affineTransform.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include<opencv2/opencv.hpp>
 
-#include <iostream>
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat dstMat;
+	cv::Mat srcMat = cv::imread("D:\\Files\\lena.jpg");//读取图像
+	if (srcMat.empty())//检测图像是否读取成功
+	{
+		return -1;
+	}
+	//变换前三点坐标
+	const cv::Point2f src_pt[] = {
+									cv::Point2f(200,200),
+									cv::Point2f(250,200),
+									cv::Point2f(200,100) };
+	//变换后三点坐标
+	const cv::Point2f dst_pt[] = {
+									cv::Point2f(300,100),
+									cv::Point2f(300,50),
+									cv::Point2f(200,100) };
+	//计算仿射矩阵
+	const cv::Mat affine_matrix = cv::getAffineTransform(src_pt, dst_pt);
+	cv::warpAffine(srcMat, dstMat, affine_matrix, srcMat.size());//进行仿射变换
+	cv::imshow("src", srcMat);//输出原图像
+	cv::imshow("dst", dstMat);//输出仿射变换后图像
+	waitKey(0);
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
